9f7a6bbb291d8840e98369707f7d0115498bceda
wxf <wxf18845574579@163.com>
2022-11-15 19:19:13 +0800

    TopN 代码实现

---
+++ HotItemsAnalysis/src/main/scala/Demo/HotItemsDemo.scala
@@ -0,0 +1,163 @@
+package Demo
+
+import java.sql.Timestamp
+import java.util.Properties
+
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.api.java.tuple.{Tuple, Tuple1}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.util.Collector
+
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved
+ *
+ * Project: UserBehaviorAnalysis
+ * Package: com.atguigu.hotitems_analysis
+ * Version: 1.0
+ *
+ * Created by wushengran on 2020/4/25 15:39
+ */
+
+// 定义输入数据的样例类
+case class UserBehavior(userId: Long, itemId: Long, categoryId: Int, behavior: String, timestamp: Long)
+
+// 定义窗口聚合结果的样例类
+case class ItemViewCount(itemId: Long, windowEnd: Long, count: Long)
+
+object HotItemsDemo {
+  def main(args: Array[String]): Unit = {
+    // 创建一个流处理执行环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    // 从文件读取数据
+    //    val inputPath: String = HotItemsDemo.getClass.getResource("UserBehavior.csv").getPath
+    val inputStream: DataStream[String] = env.readTextFile("E:\\A_data\\3.code\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv")
+
+    //    // 从kafka读取数据
+    //    val properties = new Properties()
+    //    properties.setProperty("bootstrap.servers", "hadoop104:9092")
+    //    // properties.setProperty("group.id", "consumer-group")
+    //    properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    //    properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    //    properties.setProperty("auto.offset.reset", "latest")
+    //    val inputStream: DataStream[String] = env.addSource( new FlinkKafkaConsumer[String]("hotitems", new SimpleStringSchema(), properties) )
+
+    // 将数据转换成样例类类型，并且提取timestamp定义watermark
+    val dataStream: DataStream[UserBehavior] = inputStream
+      .map(data => {
+        val dataArray = data.split(",")
+        UserBehavior(dataArray(0).toLong, dataArray(1).toLong, dataArray(2).toInt, dataArray(3), dataArray(4).toLong)
+      })
+      .assignAscendingTimestamps(_.timestamp * 1000L)
+
+    // 对数据进行转换，过滤出pv行为，开窗聚合统计个数
+    val aggStream: DataStream[ItemViewCount] = dataStream
+      .filter(_.behavior == "pv") // 过滤pv行为
+      .keyBy("itemId") // 按照itemId分组
+      .timeWindow(Time.hours(1), Time.minutes(5)) // 定义滑动窗口
+      .aggregate(new CountAgg(), new ItemCountWindowResult())
+
+    // 对窗口聚合结果按照窗口进行分组，并做排序取TopN输出
+    val resultStream: DataStream[String] = aggStream
+      .keyBy("windowEnd")
+      .process(new TopNHotItems(5))
+
+    resultStream.print()
+
+    env.execute("hot items job")
+  }
+}
+
+// 自定义预聚合函数，来一条数据就加1
+class CountAgg() extends AggregateFunction[UserBehavior, Long, Long] {
+  override def add(value: UserBehavior, accumulator: Long): Long = accumulator + 1
+
+  override def createAccumulator(): Long = 0L
+
+  override def getResult(accumulator: Long): Long = accumulator
+
+  override def merge(a: Long, b: Long): Long = a + b
+}
+
+// 扩展：自定义求平均值的聚合函数，状态为（sum，count）
+class AvgAgg() extends AggregateFunction[UserBehavior, (Long, Int), Double] {
+  override def add(value: UserBehavior, accumulator: (Long, Int)): (Long, Int) =
+    (accumulator._1 + value.timestamp, accumulator._2 + 1)
+
+  override def createAccumulator(): (Long, Int) = (0L, 0)
+
+  override def getResult(accumulator: (Long, Int)): Double = accumulator._1 / accumulator._2.toDouble
+
+  override def merge(a: (Long, Int), b: (Long, Int)): (Long, Int) =
+    (a._1 + b._1, a._2 + b._2)
+}
+
+// 自定义窗口函数，结合window信息包装成样例类
+class ItemCountWindowResult() extends WindowFunction[Long, ItemViewCount, Tuple, TimeWindow] {
+  override def apply(key: Tuple, window: TimeWindow, input: Iterable[Long], out: Collector[ItemViewCount]): Unit = {
+    val itemId = key.asInstanceOf[Tuple1[Long]].f0
+    val windowEnd = window.getEnd
+    val count = input.iterator.next()
+    out.collect(ItemViewCount(itemId, windowEnd, count))
+  }
+}
+
+
+// 自定义 KeyedProcessFunction
+class TopNHotItems(n: Int) extends KeyedProcessFunction[Tuple, ItemViewCount, String] {
+  // 定义一个ListState，用来保存当前窗口所有的count结果
+  lazy val itemCountListState: ListState[ItemViewCount] = getRuntimeContext.getListState(new ListStateDescriptor[ItemViewCount]("itemcount-list", classOf[ItemViewCount]))
+
+  override def processElement(value: ItemViewCount, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#Context, out: Collector[String]): Unit = {
+    // 每来一条数据，就把它保存到状态中
+    itemCountListState.add(value)
+    // 注册定时器，在 windowEnd + 100 触发
+    ctx.timerService().registerEventTimeTimer(value.windowEnd + 100)
+  }
+
+  // 定时器触发时，从状态中取数据，然后排序输出
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
+    // 先把状态中的数据提取到一个ListBuffer中
+    val allItemCountList: ListBuffer[ItemViewCount] = ListBuffer()
+    import scala.collection.JavaConversions._
+    for (itemCount <- itemCountListState.get()) {
+      allItemCountList += itemCount
+    }
+
+    // 按照count值大小排序，取TopN
+    val sortedItemCountList = allItemCountList.sortBy(_.count)(Ordering.Long.reverse).take(n)
+
+    // 清除状态
+    itemCountListState.clear()
+
+    // 将排名信息格式化成String，方便监控显示
+    val result: StringBuilder = new StringBuilder
+    result.append("时间：").append(new Timestamp(timestamp - 100)).append("\n")
+    // 遍历sorted列表，输出TopN信息
+    for (i <- sortedItemCountList.indices) {
+      // 获取当前商品的count信息
+      val currentItemCount = sortedItemCountList(i)
+      result.append("Top").append(i + 1).append(":")
+        .append(" 商品ID=").append(currentItemCount.itemId)
+        .append(" 访问量=").append(currentItemCount.count)
+        .append("\n")
+    }
+    result.append("==============================\n\n")
+
+    // 控制输出频率
+    Thread.sleep(1000)
+    out.collect(result.toString())
+  }
+}
---
+++ HotItemsAnalysis/src/main/scala/HotItems.scala
@@ -0,0 +1,117 @@
+import java.lang
+import java.sql.Timestamp
+
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.windowing.assigners.{SlidingEventTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+/**
+ * @Auther: wxf
+ * @Date: 2022/11/15 15:19:12
+ * @Description: HotItems  每隔5分钟输出最近一小时内 点击量 最多的前N个商品。
+ * @Version 1.0.0
+ */
+object HotItems {
+  def main(args: Array[String]): Unit = {
+
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration())
+    env.setParallelism(1)
+    // 设置时间语义 事件时间语义
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    val inputPath: String = HotItems.getClass.getResource("UserBehavior.csv").getPath
+    val inputStream: DataStream[String] = env.readTextFile(inputPath)
+
+    val mapStream: DataStream[UserBehavior] = inputStream.map(x => {
+      val str: Array[String] = x.split(",")
+      UserBehavior(str(0).toLong, str(1).toLong, str(2).toInt, str(3), str(4).toLong)
+    })
+      // 设置 waterMark 和 定义时间戳
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[UserBehavior](Time.seconds(1)) {
+        override def extractTimestamp(element: UserBehavior): Long = element.timeStamp * 1000L
+      })
+
+    // 过滤出 pv 行为，开窗聚合统计个数 计算窗口中出每个商品的点击量
+    val windowStream: DataStream[ItemViewCount] = mapStream.filter(_.behavior == "pv")
+      .keyBy(_.itemId) // 过滤 pv 行为
+      .window(SlidingEventTimeWindows.of(Time.hours(1), Time.minutes(5)))
+      .aggregate(CountAgg(), ItemCountWindowResult())
+
+    val result: DataStream[(String, Long, Long, Int)] = windowStream
+      .keyBy(_.windowEnd)
+      .process(TopNKeyedProcessFuncrion(5))
+
+    result.print("result：")
+
+    env.execute("HotItems")
+  }
+}
+
+// key -> 商品ID 可以在 全量窗口函数中获取到
+case class CountAgg() extends AggregateFunction[UserBehavior, Long, Long] {
+  override def createAccumulator(): Long = 0L
+
+  override def add(value: UserBehavior, accumulator: Long): Long = accumulator + 1
+
+  override def getResult(accumulator: Long): Long = accumulator
+
+  override def merge(a: Long, b: Long): Long = a + b
+}
+
+case class ItemCountWindowResult() extends ProcessWindowFunction[Long, ItemViewCount, Long, TimeWindow] {
+  override def process(key: Long, context: Context, elements: Iterable[Long], out: Collector[ItemViewCount]): Unit = {
+    val count: Long = elements.iterator.next()
+    out.collect(ItemViewCount(key, context.window.getEnd(), count))
+  }
+}
+
+case class TopNKeyedProcessFuncrion(rownum: Int) extends KeyedProcessFunction[Long, ItemViewCount, (String, Long, Long, Int)] {
+  // 创建一个列表状态 将输入的所有数据 进行存储
+  lazy val itemCountListState: ListState[ItemViewCount] = getRuntimeContext.getListState(new ListStateDescriptor[ItemViewCount]("list-elem", classOf[ItemViewCount]))
+
+  override def processElement(value: ItemViewCount, ctx: KeyedProcessFunction[Long, ItemViewCount, (String, Long, Long, Int)]#Context, out: Collector[(String, Long, Long, Int)]): Unit = {
+    // 将数据 添加到 listState 状态中
+    itemCountListState.add(value)
+    // 注册定时器 在 windowEnd + 1 时 触发
+    ctx.timerService().registerEventTimeTimer(value.windowEnd + 1)
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, ItemViewCount, (String, Long, Long, Int)]#OnTimerContext, out: Collector[(String, Long, Long, Int)]): Unit = {
+    // 从状态中获取 所有数据
+    val itemViewCounts: lang.Iterable[ItemViewCount] = itemCountListState.get()
+    import scala.collection.convert.wrapAll._
+    val list: List[ItemViewCount] = itemViewCounts.toList
+    // 降序排序
+    val sortedItemCountList: List[ItemViewCount] = list.sortBy(_.count)(Ordering.Long.reverse).take(rownum)
+    //      list.sortWith(_.count - _.count > 0)
+
+    // 清空状态
+    itemCountListState.clear()
+
+    // 输出
+    for (elem <- 0 until sortedItemCountList.size) {
+      val viewCount: ItemViewCount = sortedItemCountList(elem)
+      Thread.sleep(1000)
+      //        val timeStamp: String = new Timestamp(timestamp - 1).toString
+      val timeStamp: String = new Timestamp(viewCount.windowEnd).toString
+      out.collect(timeStamp, viewCount.itemId, viewCount.count, elem + 1)
+    }
+
+  }
+
+}
+
+// 定义输入数据的样例类    用户,商品，类别，行为，时间(秒)
+case class UserBehavior(userId: Long, itemId: Long, categoryId: Int, behavior: String, timeStamp: Long)
+
+// 定义窗口聚合结果的样例类
+case class ItemViewCount(itemId: Long, windowEnd: Long, count: Long)
